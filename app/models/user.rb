f7f3f3213777b44ca3a3818504be11f6d9fc7df9
Dmitry Sharov <dmitry.sharov91@gmail.com>
2019-04-24 17:29:32 +0300

    Update users registration with secret codes assigning

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,19 @@ class User < ApplicationRecord
 
   has_one :secret_code
 
-  validates :secret_code, presence: true, unless: -> { self.admin? }
+  validates :secret_code, presence: { message: 'invalid' }, unless: -> { self.admin? }
+
+  def assign_secret_code(code)
+    secret_code = SecretCode.find_by(code: code)
+
+    return unless secret_code_valid?(secret_code)
+
+    self.secret_code = secret_code
+  end
+
+  private
+
+  def secret_code_valid?(secret_code)
+    secret_code.present? && secret_code.user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, path: '', skip: 'registrations'
   devise_scope :user do
     get 'sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
-    post 'sign_up', to: 'devise/registrations#create', as: 'user_registration'
+    post 'sign_up', to: 'users/registrations#create', as: 'user_registration'
   end
 end
