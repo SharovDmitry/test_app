b958c979f44eebb90c8f3ed13dc210dcbb2b8dbc
Dmitry Sharov <dmitry.sharov91@gmail.com>
2019-04-24 14:19:59 +0300

    Add secret codes

---
+++ app/models/secret_code.rb
@@ -0,0 +1,5 @@
+class SecretCode < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :code, presence: true, uniqueness: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+
+  has_one :secret_code
+
+  validates :secret_code, presence: true, unless: -> { self.admin? }
 end
